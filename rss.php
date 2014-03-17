7aecc1d3335ea86c2d5ae73fc5b76bbd9373c689
Brian Schrader <sonictherocketman@gmail.com>
2014-03-16 20:38:56 -0700

    test

--- rss.php
+++ rss.php
@@ -1,20 +1,18 @@
   <?php
         //Outputs the RSS file for the site.
         $src = $_SERVER['DOCUMENT_ROOT'] . "/archive/metadata.json";
-        echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\t<rss version=\"2.0\">\n<channel>\n";
     	$json = json_decode(file_get_contents($src), true);
     	$num = $json["articles"];
     	if ($num != null and $num > 0) {
+	        echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\t<rss version=\"2.0\">\n<channel>\n";
 			for($i = 0; i < $i; $i++) {
-				$artdir."metadata.json";
-				 echo $artdir."\n";
-				  var_dump($json);
-				  echo "\t<item>\n
-				  <title>".$json["title".$i]."</title>\n
-				  <link>".$json["link".$i]."</link>\n
-				  <description>".substr(strip_tags($json["content".$i]), 0, 20)."...</description>\n
-				  </item>\n";
+			  var_dump($json);
+			  echo "\t<item>\n
+			  <title>".$json["title".$i]."</title>\n
+			  <link>".$json["link".$i]."</link>\n
+			  <description>".substr(strip_tags($json["content".$i]), 0, 20)."...</description>\n
+			  </item>\n";
 			 }
+        echo "</channel>\n</rss>";
 		}
-        echo "</rss>";
  ?>
