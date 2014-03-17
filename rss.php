0c1bcdac98d8261d221185f593c47436cb09fe10
Brian Schrader <sonictherocketman@gmail.com>
2014-03-16 21:10:58 -0700

    To pass validation

--- rss.php
+++ rss.php
@@ -1,21 +1,24 @@
   <?php
+  echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
         //Outputs the RSS file for the site.
         $src = $_SERVER['DOCUMENT_ROOT'] . "/archive/metadata.json";
     	$json = json_decode(file_get_contents($src), true);
     	$num = $json["articles"];
     	if ($num != null and $num > 0) {
-	        echo "<rss xmlns:blogChannel=\"http://brianschrader.com\" version=\"2.0\">\n<channel>\n";
+	        echo "<rss version=\"2.0\">\n<channel>\n";
+			echo "<title>BiteofanApple by Brian Schrader</title> \n
+				  <link>http://brianschrader.com</link>\n
+				  <desciption>A blog by Brian Schrader.</description>"
 			for($i = 0; $i < $num; $i++) {
 			  //$articleJson = file_get_contents($json['link'.$i]."metadata.json");
 			  //$artJs = json_decode($articleJson);
 			  //$content = substr(strip_tags($articleJson['content']), 0, 20);
 			  echo "\t<item>\n
-			  <title>".$json['title'.$i]."</title>\n
-			  <link>".$json['link'.$i]."</link>\n
-			  <pubdate>".$json['published'.$i]."</pubdate>\n
-			  <description>"./*$content."..."*/ ""."</description>\n
-			  <author>".$json['author'.$i]."</author>\n
-			  </item>\n";
+			 	<title>".$json['title'.$i]."</title>\n
+			    <link>".$json['link'.$i]."</link>\n
+			  	<published>".$json['published'.$i]."</published>\n
+			  	<description>"./*$content."..."*/ ""."</description>\n
+			  	</item>\n";
 			 }
         echo "</channel>\n</rss>";
 		}
