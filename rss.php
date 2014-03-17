a47b3fb3e99d3d2c419a6509c15628d34636979d
Brian Schrader <sonictherocketman@gmail.com>
2014-03-16 20:35:30 -0700

    Hopeful fix

--- rss.php
+++ rss.php
@@ -1,16 +1,21 @@
-<?php
-	//Outputs the RSS file for the site.
-	$dir = $_SERVER['DOCUMENT_ROOT'] . "/archive/";
-	$data = array();
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\t<rss version=\"2.0\">\n<channel>";
-	while($dirs = glob($dir, GLOB_ONLYDIR)) {
-		$dir."/metadata.json";	
-		$json = json_decode(file_get_contents($dir));
-		echo "<item>\n
-		<title>".$json['title']."</title>\n
-		<link>".$json['link']."</link>\n
-		<description>".substr($json['content'], 0, 30)."...<\description>\n
-		</item>";
-   	}
-   	echo "</rss>";
-?>
+  <?php
+        //Outputs the RSS file for the site.
+        $src = $_SERVER['DOCUMENT_ROOT'] . "/archive/metadata.json";
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\t<rss version=\"2.0\">\n<channel>\n";
+    	$json = json_decode(file_get_contents($src), true);
+    	$num = $json["articles"];
+    	if ($num != null and $num > 0) {
+			for($i = 0; i < $i; $i++) {
+				$artdir."metadata.json";
+				 echo $artdir."\n";
+				  if (file_exists($artdir)){      
+				  var_dump($json);
+				  echo "\t<item>\n
+				  <title>".$json["title".$i]."</title>\n
+				  <link>".$json["link".$i]."</link>\n
+				  <description>".substr(strip_tags($json["content".$i]), 0, 20)."...</description>\n
+				  </item>\n";
+			 }
+		}
+        echo "</rss>";
+ ?>
