40049cc39fa6574741db788ecba5b5fd73c69133
Brian Schrader <sonictherocketman@gmail.com>
2014-03-16 20:43:37 -0700

    stupid error in RSS.php

--- rss.php
+++ rss.php
@@ -7,12 +7,12 @@
     	if ($num != null and $num > 0) {
     		echo $num;
 	        echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\t<rss version=\"2.0\">\n<channel>\n";
-			for($i = 0; i < $i; $i++) {
+			for($i = 0; $i < $num; $i++) {
 			  var_dump($json);
 			  echo "\t<item>\n
-			  <title>".$json["title".$i]."</title>\n
-			  <link>".$json["link".$i]."</link>\n
-			  <description>".substr(strip_tags($json["content".$i]), 0, 20)."...</description>\n
+			  <title>".$json['title'.$i]."</title>\n
+			  <link>".$json['link'.$i]."</link>\n
+			  <description>".substr(strip_tags($json['content'.$i]), 0, 20)."...</description>\n
 			  </item>\n";
 			 }
         echo "</channel>\n</rss>";
