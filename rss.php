16249b6de805f12684e80e4d871ecc205b59619c
Brian Schrader <sonictherocketman@gmail.com>
2014-03-16 21:00:05 -0700

    Now it should work...

--- rss.php
+++ rss.php
@@ -6,15 +6,15 @@
     	if ($num != null and $num > 0) {
 	        echo "<rss xmlns:blogChannel=\"http://brianschrader.com\" version=\"2.0\">\n<channel>\n";
 			for($i = 0; $i < $num; $i++) {
-			  $articleJson = file_get_contents($json['link'.$i]."metadata.json");
-			  $artJs = json_decode($articleJson);
-			  $content = substr(strip_tags($articleJson['content']), 0, 20);
+			  //$articleJson = file_get_contents($json['link'.$i]."metadata.json");
+			  //$artJs = json_decode($articleJson);
+			  //$content = substr(strip_tags($articleJson['content']), 0, 20);
 			  echo "\t<item>\n
 			  <title>".$json['title'.$i]."</title>\n
 			  <link>".$json['link'.$i]."</link>\n
 			  <pubdate>".$json['published'.$i]."</pubdate>\n
 			  <description>".$content."...</description>\n
-			  <author>".$json['author'.$i]."</author>\n
+			  <author>"./*$json['author'.$i]*/ ""."</author>\n
 			  </item>\n";
 			 }
         echo "</channel>\n</rss>";
