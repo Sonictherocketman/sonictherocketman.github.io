43de1d7938912d33bc4cd954ac0198e8e14eed97
Brian Schrader <sonictherocketman@gmail.com>
2014-03-16 21:34:26 -0700

    NOW it should work...

--- rss.php
+++ rss.php
@@ -1,14 +1,14 @@
   <?php
   header('Content-type: text/xml');
-  echo "<!DOCTYPE rss>
+  $xml = "<!DOCTYPE rss>
 		<?xml version=\"1.0\" ?>\n";
         //Outputs the RSS file for the site.
         $src = $_SERVER['DOCUMENT_ROOT'] . "/archive/metadata.json";
     	$json = json_decode(file_get_contents($src), true);
     	$num = $json["articles"];
     	if ($num != null and $num > 0) {
-	        echo "<rss version=\"2.0\">\n<channel>\n";
-			echo "<title>BiteofanApple by Brian Schrader</title> \n
+	        $xml .= "<rss version=\"2.0\">\n<channel>\n";
+			$xml .= "<title>BiteofanApple by Brian Schrader</title> \n
 				  <link>http://brianschrader.com</link>\n
 				  <desciption>A blog by Brian Schrader.</description>\n
 				  <language>en-us</language>\n
@@ -17,7 +17,7 @@
 			  //$articleJson = file_get_contents($json['link'.$i]."metadata.json");
 			  //$artJs = json_decode($articleJson);
 			  //$content = substr(strip_tags($articleJson['content']), 0, 20);
-			  echo "\t<item>\n
+			  $xml .= "\t<item>\n
 			 	<title>".$json['title'.$i]."</title>\n
 			    <link>".$json['link'.$i]."</link>\n
 			  	<pubDate>".$json['published'.$i]."</pubDate>\n
@@ -25,6 +25,8 @@
 			  	<guid>".$json['link'.$i].$json['published']."</guid>\n
 			  	</item>\n";
 			 }
-        echo "</channel>\n</rss>";
+        $xml .= "</channel>\n";
 		}
+		$xml .= "</rss>"; 
+		echo $xml
  ?>
