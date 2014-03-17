82b2f1fc23a7f18b73ee98b2039cca701b5ee731
Brian Schrader <sonictherocketman@gmail.com>
2014-03-16 20:06:50 -0700

    ...again...fixed i think...

--- rss.php
+++ rss.php
@@ -1,6 +1,6 @@
 <?php
 	//Outputs the RSS file for the site.
-	$dir = "archive/";
+	$dir = $_SERVER['DOCUMENT_ROOT'] . "/archive/";
 	$data = array();
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\t<rss version=\"2.0\">\n<channel>";
 	while($dirs = glob($dir, GLOB_ONLYDIR)) {
