8a7b03061751d63baf4591a923a8b904128c99e2
Brian Schrader <sonictherocketman@gmail.com>
2014-03-16 20:47:38 -0700

    I think I fixed it...

--- rss.php
+++ rss.php
@@ -3,12 +3,9 @@
         $src = $_SERVER['DOCUMENT_ROOT'] . "/archive/metadata.json";
     	$json = json_decode(file_get_contents($src), true);
     	$num = $json["articles"];
-    	var_dump($json);
     	if ($num != null and $num > 0) {
-    		echo $num;
 	        echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\t<rss version=\"2.0\">\n<channel>\n";
 			for($i = 0; $i < $num; $i++) {
-			  var_dump($json);
 			  echo "\t<item>\n
 			  <title>".$json['title'.$i]."</title>\n
 			  <link>".$json['link'.$i]."</link>\n
