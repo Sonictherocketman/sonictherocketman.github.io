e9400fc77fe7a6d2ede0d17847ca803c9d000b62
Brian Schrader <sonictherocketman@gmail.com>
2014-03-16 20:04:11 -0700

    ...

--- rss.php
+++ rss.php
@@ -3,7 +3,7 @@
 	$dir = "archive/";
 	$data = array();
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\t<rss version=\"2.0\">\n<channel>";
-	while($dirs = glob($dir . '/*', GLOB_ONLYDIR)) {
+	while($dirs = glob($dir, GLOB_ONLYDIR)) {
 		$dir."/metadata.json";	
 		$json = json_decode(file_get_contents($dir));
 		echo "<item>\n
